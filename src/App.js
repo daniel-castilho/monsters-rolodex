417d68b0b849082f7bda6726b24c8693e271798d
Daniel Castilho <dan.castilho@gmail.com>
2021-05-28 14:17:06 -0400

    Fetching Content

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import React, { Component } from "react";
+
+class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			monsters: [],
+		};
+	}
+
+	componentDidMount() {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((res) => res.json())
+			.then((users) => this.setState({ monsters: users }));
+	}
+
+	render() {
+		return (
+			<div className="App">
+				{this.state.monsters.map((monster) => (
+					<h1 key={monster.id}> {monster.name}</h1>
+				))}
+			</div>
+		);
+	}
+}
+
+export default App;
