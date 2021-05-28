96ea8bd7588693eb55899b921e950c39c606e415
Daniel Castilho <dan.castilho@gmail.com>
2021-05-28 15:01:10 -0400

    Create Component Card-List

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
+import Card from "../card/card.component";
+
+import "./card-list.styles.css";
+
 const CardList = (props) => {
-	return <h1>CardList</h1>;
+	return (
+		<div className="card-list">
+			{props.monsters.map((monster) => (
+				<Card key={monster.id} monster={monster} />
+			))}
+		</div>
+	);
 };
 
 export default CardList;
