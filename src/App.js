e79d75808f5b0bafaac988959809325f2fa349ba
Daniel Castilho <dan.castilho@gmail.com>
2021-05-28 23:23:47 -0400

    CSS Adjusts

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ class App extends Component {
 			monsters: [],
 			searchField: "",
 		};
+
+		//this.handleChange = this.handleChange.bind(this);
 	}
 
 	componentDidMount() {
@@ -18,12 +20,18 @@ class App extends Component {
 			.then((users) => this.setState({ monsters: users }));
 	}
 
+	handleChange = (e) => {
+		this.setState({ searchField: e.target.value });
+	}
+
 	render() {
 		const { monsters, searchField } = this.state;
 		const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
 		return (
 			<div className="App">
-				<SearchBox placeholder='search monsters' handleChange={(e) => this.setState({ searchField: e.target.value })} />
+				<h1>Monsters Rolodex</h1>
+				<SearchBox placeholder='search monsters'
+					handleChange={this.handleChange} />
 				<CardList monsters={ filteredMonsters } />
 			</div>
 		);
